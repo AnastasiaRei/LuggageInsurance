d2d041370894749d1b34a2cbde4da5a7f7dc74cd
Anastasia Reimer <anastasiareimer5@googlemail.com>
2019-02-23 20:17:42 +0100

    added struct Flight, added function setFlight, added setFlightStatus

--- LuggageInsurance.sol
+++ LuggageInsurance.sol
@@ -1,43 +1,45 @@
 pragma solidity ^0.5.4;
 
 contract LuggageInsuranceContract {
+    
+    struct Flight{
+        string flightNumber;
+        uint departureTime;
+        uint arrivalTime;
+        string departureAirport;
+        bool landed;
+    }
+    
+    Flight public flight;
     address addressInsurance;
     address addressInsuree;
     address addressOracle = 0x72C396cBE08ed7cE86EB84C2E25Cd6800aC7d7f4;
-    /*
-    string flightNumber;
-    uint departureTime;
-    uint arrivalTime;
-    string departureAirport;
-    */
     uint premium= 100 wei;
     string public status;
     uint public balance;
     string public luggageID;
         
-    constructor(
-        address _addressInsuree
-        /*
-        string memory _flightNumber,
-        uint _departureTime,
-        uint _arrivalTime,
-        string memory _departureAirport
-        */
-    ) public payable{
+    constructor(address _addressInsuree) public payable{
         addressInsuree = _addressInsuree;
         addressInsurance = msg.sender;
         status = "initialized";
-        /*
-        flightNumber = _flightNumber;
-        departureTime = _departureTime;
-        arrivalTime = _arrivalTime;
-        departureAirport = _departureAirport;
-        */
+    }
+    
+    function setFlight(
+        string memory flightNumber,
+        uint departureTime,
+        uint arrivalTime,
+        string memory departureAirport
+    ) public {
+        require(msg.sender == addressInsurance);
+        require(compareStrings(status, "initialized"));
+        flight = Flight(flightNumber, departureTime, arrivalTime, departureAirport,false);
+        status = "flightSet";
     }
     
     function payPremium() public payable{
         require(msg.sender == addressInsuree);
-        require(compareStrings(status, "initialized"));
+        require(compareStrings(status, "flightSet"));
         require(msg.value == premium);
         balance += msg.value;
         status = "paid";
@@ -55,6 +57,17 @@ contract LuggageInsuranceContract {
         require(_addressInsuree == addressInsuree);
         require(compareStrings(status, "checkedIn"));
         status = "boarded";
+    }
+    
+    function setFlightStatus(string memory flightStatus) public{
+        require(msg.sender == addressOracle);
+        require(compareStrings(status, "boarded"));
+        if(compareStrings(flightStatus, "landed")){
+            status = "landed";
+            flight.landed = true;
+        }else{
+            //ask oracle again in some time
+        }
     }
 
     function compareStrings(string memory a, string memory b) internal pure returns (bool){
