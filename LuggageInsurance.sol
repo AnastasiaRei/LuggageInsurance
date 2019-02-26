065d216eea8e1124057e1ae9138c618bb6768cba
Anastasia Reimer <anastasiareimer5@googlemail.com>
2019-02-26 23:19:42 +0100

    new structure enum State, add Insuree struct

--- LuggageInsurance.sol
+++ LuggageInsurance.sol
@@ -2,85 +2,98 @@ pragma solidity ^0.5.4;
 
 contract LuggageInsuranceContract {
     
-    struct Flight{
+    struct Flight {
         string flightNumber;
         uint departureDay;
         bool landed;
         uint timeLanded;
+        bool initialized;
     }
     
-    struct Luggage{
+    struct Luggage {
         string id;
         bool onBelt; 
         uint timeOnBelt;
+        bool initialized;
+    }
+    
+    struct Insuree {
+        bool boarded;
+        address payable addressInsuree;
+    }
+    
+    enum State {
+        inactive, active, revoked, closed
     }
     
     Flight public flight;
     Luggage public luggage;
+    Insuree public insuree;
     address payable addressInsurance = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
-    address payable addressInsuree;
     address addressOracle = 0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
     uint premium= 5 ether;
-    string public status;
+    State public status;
     uint public balance;
     uint public timeDifference;
     //in Sec
     uint public timeLimitForPayOut = 30;
         
     constructor() public payable{
-        addressInsuree = msg.sender;
-        status = "initialized";
+        insuree = Insuree(false, msg.sender);
+        status = State.inactive;
     }
     
     function setFlight(
         string memory flightNumber,
         uint departureDay
     ) public {
-        require(msg.sender == addressInsuree);
-        require(compareStrings(status, "initialized"));
+        require(msg.sender == insuree.addressInsuree);
+        require(status == State.inactive);
         flight = Flight(
             flightNumber,
             departureDay,
             false,
-            0
+            0, 
+            true
         );
-        status = "flightSet";
     }
     
     function payPremium() public payable{
-        require(msg.sender == addressInsuree);
-        require(compareStrings(status, "flightSet"));
+        require(msg.sender == insuree.addressInsuree);
+        require(flight.initialized);
+        require(status == State.inactive);
         require(msg.value == premium);
         balance += msg.value;
-        status = "paid";
+        status = State.active;
     }
 
     function checkInLuggage(string memory _luggageID) public{
         require(msg.sender == addressOracle);
-        require(compareStrings(status, "paid"));
-        luggage = Luggage(_luggageID, false, 0);
-        status = "checkedIn";
+        require(status == State.active);
+        require(!luggage.initialized);
+        luggage = Luggage(_luggageID, false, 0, true);
     }
     
     function revokeContract() public{
-        require(msg.sender == addressInsuree);
-        require(compareStrings(status, "checkedIn") || compareStrings(status, "paid"));
-        addressInsuree.transfer(balance);
-        status = "revoked";
+        require(msg.sender == insuree.addressInsuree);
+        require(status == State.active);
+        require(!insuree.boarded);
+        insuree.addressInsuree.transfer(balance);
+        status = State.revoked;
     }
     
     function boardingPassenger(address _addressInsuree) public{
         require(msg.sender == addressOracle);
-        require(_addressInsuree == addressInsuree);
-        require(compareStrings(status, "checkedIn"));
-        status = "boarded";
+        require(_addressInsuree == insuree.addressInsuree);
+        require(luggage.initialized);
+        require(!insuree.boarded);
+        insuree.boarded = true;
     }
     
     function setFlightStatus(string memory flightStatus) public{
         require(msg.sender == addressOracle);
-        require(compareStrings(status, "boarded"));
+        require(insuree.boarded);
         if(compareStrings(flightStatus, "landed")){
-            status = "landed";
             flight.landed = true;
             flight.timeLanded = now;
         }else {
@@ -89,11 +102,11 @@ contract LuggageInsuranceContract {
     }
     
     function setLuggageStatus(string memory _luggageID, bool _onBelt) public{
+        require(status == State.active);
         require(msg.sender == addressOracle);
-        require(compareStrings(status, "landed"));
+        require(flight.landed);
         require(compareStrings(_luggageID, luggage.id));
         if(_onBelt == true){
-            status = "onBelt";
             luggage.onBelt = true;
             luggage.timeOnBelt = now;
             checkClaim();
@@ -101,16 +114,17 @@ contract LuggageInsuranceContract {
     }
     
     function checkClaim() private {
-        // require(flight.landed == true);
+        require(status == State.active);
+        require(flight.landed);
         // check both cases of delay and lost
         if(luggage.onBelt) {
             timeDifference = luggage.timeOnBelt - flight.timeLanded;
             if (timeDifference > timeLimitForPayOut) {
-                addressInsuree.transfer(balance);
-                status = "claimPaid";
+                insuree.addressInsuree.transfer(balance);
+                status = State.closed;
             } else {
                 addressInsurance.transfer(balance);
-                status = "noClaimPaid";
+                status = State.closed;
             }
         }
     }
